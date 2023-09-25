836955340158f0aa2ec3b01a8a3b834607050dbc
morispolanco <morispolanco@gmail.com>
2023-09-25 15:29:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,36 @@
 import streamlit as st
-import requests
-import re
+import openai
 
-def web_tech_detector(url):
-    # Realizar la solicitud GET al sitio web
-    response = requests.get(url)
+# Configurar la API de OpenAI
+openai.api_key = st.text_input("Ingrese su API Key de OpenAI")
+
+def analyze_website(url):
+    # Realizar el análisis utilizando GPT-3
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"Analiza la URL: {url}.",
+        max_tokens=500,
+        n=1,
+        stop=None,
+        temperature=0.5
+    )
     
-    # Verificar si la solicitud fue exitosa
-    if response.status_code == 200:
-        # Obtener el contenido HTML de la página
-        html_content = response.text
-        
-        # Buscar pistas sobre el lenguaje de programación y la plataforma utilizados
-        language = re.findall(r'<script.*?src=["\'](.*?)["\']', html_content)
-        platform = re.findall(r'<link.*?href=["\'](.*?)["\']', html_content)
-        
-        # Mostrar los resultados en la aplicación de Streamlit
-        st.write("Lenguaje de programación detectado:")
-        st.write(set(language))
-        
-        st.write("Plataforma detectada:")
-        st.write(set(platform))
-    else:
-        st.write("Error al acceder al sitio web")
+    # Obtener la respuesta generada por GPT-3
+    analysis = response.choices[0].text.strip()
+    
+    # Mostrar el informe en la aplicación de Streamlit
+    st.write("Informe de análisis del sitio web:")
+    st.write(analysis)
 
 # Configurar la interfaz de la aplicación de Streamlit
-st.title("Detector de tecnología web")
+st.title("Analizador de sitios web")
 url = st.text_input("Ingrese la URL del sitio web")
 
 # Verificar si se ha ingresado una URL
 if url:
-    # Llamar a la función de detección de tecnología web
-    web_tech_detector(url)
+    # Verificar si se ha ingresado una API Key
+    if openai.api_key:
+        # Llamar a la función de análisis del sitio web
+        analyze_website(url)
+    else:
+        st.write("Por favor, ingrese su API Key de OpenAI")
